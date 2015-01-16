6c4d68f4e17a240df1f60ae1746561fb4dc9e0db
James Vodanovich <james@jvodan.com>
2015-01-16 18:58:30 +1100

    first commit

--- engines/scripts/post_install.sh
+++ engines/scripts/post_install.sh
@@ -1,3 +1,3 @@
-mkdir -p /home/fs/wordpress/app/
-mv /home/app/wp-config.php /home/fs/wordpress/app/
-ln -s /home/fs/wordpress/app/wp-config.php /home/app/
+mkdir -p /home/fs/wordpress/app/;
+mv /home/app/wp-config.php /home/fs/wordpress/app/;
+ln -s /home/fs/wordpress/app/wp-config.php /home/app/;

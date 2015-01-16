5bfb5b08ad489a82282327d41678fa7263b08373
James Vodanovich <james@jvodan.com>
2015-01-16 12:43:28 +1100

    first commit

--- engines/templates/wp-config.php
+++ engines/templates/wp-config.php
@@ -59,7 +59,7 @@ define('NONCE_SALT',       'put your unique phrase here');
  * You can have multiple installations in one database if you give each a unique
  * prefix. Only numbers, letters, and underscores please!
  */
-$table_prefix  = '';
+$table_prefix  = 'wp_';
 define( 'WP_SITEURL', 'http://_ENGINES_fqdn/' );
 /**
  * For developers: WordPress debugging mode.
